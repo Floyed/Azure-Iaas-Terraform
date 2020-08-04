14842be26e3d55d5e6f2507c58e121b46d40ac15
Floyed Pinto <pinto.fl@husky.neu.edu>
2020-08-03 20:24:19 -0400

    Orphan commit

---
+++ main.tf
@@ -0,0 +1,117 @@
+variable "StackName" {
+  default = "Floyed-webapp"
+} 
+
+variable "Region" {
+  default = "East US"
+} 
+
+variable "PubKeyData" {
+  default = ""
+} 
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.StackName}-resources"
+  location = "${var.Region}"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.StackName}-network"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  location            = "${azurerm_resource_group.main.location}"
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_network_security_group" "main" {
+  name                = "${var.StackName}-security-group"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "SSH_port"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = "internal"
+  virtual_network_name = "${azurerm_virtual_network.main.name}"
+  resource_group_name  = "${azurerm_resource_group.main.name}"
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.StackName}-nic"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Static"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "main" {
+  subnet_id                 = azurerm_subnet.main.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
+
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.StackName}-vm"
+  location              = azurerm_resource_group.main.location
+  resource_group_name   = azurerm_resource_group.main.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_B1ls"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "${var.StackName}-ubuntu"
+    admin_username = "ubuntu"
+    admin_password = "admin"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+      path = "/home/ubuntu/.ssh/authorized_keys"
+      key_data = "${var.PubKeyData}"
+    }
+    
+  }
+  tags = {
+    environment = "staging"
+  }
+}
